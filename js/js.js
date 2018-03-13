569ffa7a1331c101187211b3274787742e64f551
suchenxiao <suchenxiao@foxmail.com>
2018-03-13 20:15:51 +0800

    Feature： 增加猫咪等级系统

--- js/js.js
+++ js/js.js
@@ -7,6 +7,14 @@ $(function(){
 		this.incrementCounter = function() {
 			this.clickCount(this.clickCount()+1);
 		};
+
+		this.level = ko.computed(function(){
+			if(this.clickCount()<=5) return 'LEVEL-1';
+			else if(this.clickCount()<=10) return 'LEVEL-2';
+			else if(this.clickCount()<=15) return 'LEVEL-3';
+			else if(this.clickCount()<=20) return 'LEVEL-4';
+			else return 'LEVEL-5';
+		}, this);
 	}
 	
 	ko.applyBindings(new ViewModel());
