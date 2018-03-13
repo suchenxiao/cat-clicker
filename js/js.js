0f8f8d25c62a17c361f6662c2bafb3e91f366bfe
suchenxiao <suchenxiao@foxmail.com>
2018-03-13 21:06:36 +0800

    Refactor： with绑定上下文

--- js/js.js
+++ js/js.js
@@ -16,9 +16,10 @@ $(function(){
 	};
 
 	var ViewModel = function() {
+		var self = this;
 		this.currentCat = ko.observable(new Cat());
 		this.incrementCounter = function() {
-			this.currentCat().clickCount(this.currentCat().	clickCount()+1);
+			self.currentCat().clickCount(self.currentCat().clickCount()+1);
 		};
 
 	};
